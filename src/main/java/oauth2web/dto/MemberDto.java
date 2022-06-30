1a7327dcc6af0b0971a767db64ce79955bb37eb1
kimmyuung <rlaaudgh1003@daum.net>
2022-06-30 13:51:03 +0900

    ㅇ

--- src/main/java/oauth2web/dto/MemberDto.java
+++ src/main/java/oauth2web/dto/MemberDto.java
@@ -18,8 +18,8 @@ public class MemberDto {
     private String mpassword;
     private Role role;
 
-    public MemberEntity toentity() {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    public MemberEntity toentity() { // 멤버 DTO를 엔티티로 변환
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(); // 보안 위해 패스워드 인코딩
         return MemberEntity.builder()
                 .mid(this.mid)
                 .mpassword(encoder.encode(this.mpassword))
