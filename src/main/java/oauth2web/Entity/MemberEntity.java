a3160991bfc5a270a295cfaef96ee8c63dd7d385
kimmyuung <rlaaudgh1003@daum.net>
2022-06-30 13:58:08 +0900

    주석ㅇㅇ

--- src/main/java/oauth2web/Entity/MemberEntity.java
+++ src/main/java/oauth2web/Entity/MemberEntity.java
@@ -2,10 +2,7 @@ package oauth2web.Entity;
 
 import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity // 엔티티로 사용
 @AllArgsConstructor@NoArgsConstructor // 모든 필드를 가진 생성자와 텅빈 필드를 가진 생성자 생성
@@ -18,5 +15,6 @@ public class MemberEntity {
 
     private String mid;
     private String mpassword;
+    @Enumerated(EnumType.STRING) // 열거형 객체를 문자열로 표현
     private Role role;
 }
